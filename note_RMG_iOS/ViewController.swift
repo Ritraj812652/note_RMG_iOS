6b619275bcb4b2ac559c86b25b828cb6f428f9d1
Ritraj <c0812652@mylambton.ca>
2021-09-21 19:57:04 -0400

    Initial Commit

---
+++ note_RMG_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  note_RMG_iOS
+//
+//  Created by Ritraj Singh on 21/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
