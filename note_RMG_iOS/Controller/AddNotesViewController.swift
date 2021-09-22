d07d1f828dcc29bb083a45b24cc71cdecfed1af4
GurpreetKaurSandhu <c0823239@mylambton.ca>
2021-09-22 03:44:09 -0400

    created AddNotesViewController

---
+++ note_RMG_iOS/Controller/AddNotesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddNotesViewController.swift
+//  note_RMG_iOS
+//
+//  Created by Gurpreet Kaur on 22/09/21.
+//
+
+import UIKit
+
+class AddNotesViewController: UIViewController {
+
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var notesTextField: UITextView!
+    @IBOutlet weak var addedImage: UIImageView!
+    @IBOutlet weak var recordAudioImg: UIImageView!
+    @IBOutlet weak var playAudioButton: UIButton!
+    @IBOutlet weak var playAudioImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
