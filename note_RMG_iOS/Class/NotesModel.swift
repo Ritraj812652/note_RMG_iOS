673e94c694768f856e9ba77ae3b9c07aeb51a829
Suresh8723 <c0818723@mylambton.ca>
2021-09-22 01:18:01 -0400

    Added notesModel and updated the coreData

---
+++ note_RMG_iOS/Class/NotesModel.swift
@@ -0,0 +1,42 @@
+//
+//  NotesModel.swift
+//  note_RMG_iOS
+//
+//  Created by Suresh on 2021-09-22.
+//
+
+import CoreData
+import UIKit
+
+class NotesModel {
+    let createdDate: Double
+    let editedDate: Double
+    let latitude: Double
+    let longitude: Double
+    let primaryKey: Double
+    let noteDesc: String
+    let title: String
+    let image: Data
+    
+    init(_ data: NSManagedObject) {
+        self.title = data.value(forKey: "title") as! String
+        self.noteDesc = data.value(forKey: "noteDesc") as! String
+        self.createdDate = data.value(forKey: "createdDate") as! Double
+        self.editedDate = data.value(forKey: "editedDate") as! Double
+        self.latitude = data.value(forKey: "latitude") as! Double
+        self.longitude = data.value(forKey: "longitude") as! Double
+        self.primaryKey = data.value(forKey: "primaryKey") as! Double
+        self.image = data.value(forKey: "image") as? Data ?? Data()
+    }
+    
+    init(title: String, noteDesc: String, createdDate: Double, editedDate: Double, latitude: Double, longitude: Double, primaryKey: Double, image: Data) {
+        self.title = title
+        self.noteDesc = noteDesc
+        self.createdDate = createdDate
+        self.editedDate = editedDate
+        self.latitude = latitude
+        self.longitude = longitude
+        self.primaryKey = primaryKey
+        self.image = image
+    }
+}
