f8a951e078571b501c47094a8cfc3be039165eff
Suresh8723 <c0818723@mylambton.ca>
2021-09-22 13:36:35 -0400

    added extension for image

--- note_RMG_iOS/Helper/extension.swift
+++ note_RMG_iOS/Helper/extension.swift
@@ -26,3 +26,18 @@ extension Date {
         return Double(self.timeIntervalSince1970)
     }
 }
+
+
+extension UIImage {
+    func fixOrientation() -> UIImage? {
+        if self.imageOrientation == UIImage.Orientation.up {
+            return self
+        }
+
+        UIGraphicsBeginImageContext(self.size)
+        self.draw(in: CGRect(origin: .zero, size: self.size))
+        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return normalizedImage
+    }
+}
