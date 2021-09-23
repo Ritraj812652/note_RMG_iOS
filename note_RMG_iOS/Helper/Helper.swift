4e32837ac78bbc7bf2f41afd795d0b73c91bd076
GurpreetKaurSandhu <c0823239@mylambton.ca>
2021-09-23 17:10:29 -0400

    added getNoteBackColor to helper class

--- note_RMG_iOS/Helper/Helper.swift
+++ note_RMG_iOS/Helper/Helper.swift
@@ -21,6 +21,9 @@ class Helper {
         dateFormatter.timeZone = TimeZone.current
         return dateFormatter.string(from: dateVar)
     }
-    
+    func getNoteBackColor() -> [UIColor] {
+        let colorArray = [UIColor(rgb: 0x5EBD3E), UIColor(rgb: 0xFFB900), UIColor(rgb: 0xF78200), UIColor(rgb: 0xE23828), UIColor(rgb: 0x973999), UIColor(rgb: 0x009CDF)]
+        return colorArray
+    }
     
 }
